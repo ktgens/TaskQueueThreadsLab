6ae9bf0d78963ed7c3d0cca4362a6b506e51d2e6
ktgent <hlebevol@gmail.com>
2026-05-07 14:17:43 +0300

    Add project files.

---
+++ TaskQueueThreads.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <mutex>
+#include <condition_variable>
+
+class TaskQueue
+{
+public:
+    void AddTask()
+    {
+
+    }
+
+    void RemoveTask()
+    {
+
+    }
+};
+
+int main()
+{
+}
+
